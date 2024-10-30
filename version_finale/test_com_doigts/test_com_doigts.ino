5ccdb426ee5b964ff92a990ebbf8a541fcf90d5d
Philippe <philippe.gauthier@krag.io>
2024-10-30 16:16:17 -0400

    retro

--- version_finale/test_com_doigts/test_com_doigts.ino
+++ version_finale/test_com_doigts/test_com_doigts.ino
@@ -2,14 +2,56 @@
 #include <ICM20948_WE.h>
 #include <Wire.h>
 
+#define PCF8574_ADDRESS 0x20
 bool spi = true;
-
+bool hapticUpdate = false;
 const int NUMSENSORS = 8;
 const int MSG_BUFFER_SIZE = 34;
 
 const int HEART_BEAT_PIN = 5;
 
 float sensorData[48];
+byte hapticData = 0x00;
+
+void updateHaptic(){
+  Wire.beginTransmission(PCF8574_ADDRESS);
+  Wire.write(~(hapticData));  // Write the byte to the PCF8574
+  Wire.endTransmission();  // End the transmission
+  }
+  
+void updateHapticData(int id, bool flag1, bool flag2){
+  
+  byte mask = 0b00000000;
+  switch(id){
+  
+  case 3:
+  mask = 0b10001000;
+  break;
+
+  case 4:
+  mask = 0b01000100;
+  break;
+
+  case 5:
+  mask = 0b00100010;
+  break;
+
+  case 6:
+  mask = 0b00010001;
+  break;
+
+  default:
+  mask = 0b00000000;
+  break;
+  
+  }
+  if(flag1){hapticData |= mask & 0x0F ;}
+  else{hapticData &= ~(mask & 0x0F) ;}
+  if(flag2){hapticData |= mask & 0xF0 ;}
+  else{hapticData &= ~(mask & 0xF0) ;}
+  
+}
+  
 
 ICM20948_WE IMU_AVANT_BRAS = ICM20948_WE(37,spi);
 ICM20948_WE IMU_MAIN = ICM20948_WE(35,spi);
@@ -24,8 +66,8 @@ ICM20948_WE sensors[NUMSENSORS] = {
   IMU_AVANT_BRAS, IMU_MAIN, IMU_EPAULE, IMU_INDEX, IMU_MAJEUR,IMU_ANNU ,IMU_AURI, IMU_POUCE
 };
 
-
 char message[64] = {};
+char messageDebug[64] = {};
 void update_mux(int id){
   switch(id){
     case 0:
@@ -112,6 +154,8 @@ void update_mux(int id){
 }
 void setup() {
   Serial.begin(115200);
+  Wire.begin(17, 18);
+  updateHaptic();
   while(!Serial) {}
 
   // heart beat pin 
@@ -178,5 +222,17 @@ void loop() {
     sprintf(message,"%d %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f", i, acc.x, acc.y, acc.z, gyr.x, gyr.y, gyr.z);
     Serial.println(message);
   }
+  if (Serial.available() >= 3) {  // Check if we have enough bytes (ID, 2 flags)
+    char id = Serial.read();       // Read the ID character
+    char flag1 = Serial.read();    // Read the first boolean character
+    char flag2 = Serial.read();    // Read the second boolean character
+
+    // Convert characters to integers
+    int intId = id - '0';          // Convert '0' to '9' to int 0 to 9
+    bool boolFlag1 = flag1 - '0';  // Convert '0' or '1' to boolean
+    bool boolFlag2 = flag2 - '0';
+    updateHapticData(intId,boolFlag1,boolFlag2);
+  }
+  updateHaptic();
   delay(50);
 }
